f95cf7428e524476dbb23bf86b5011ff4226c261
Vincent Dagpin <vincent.dagpin@gmail.com>
2021-12-09 08:10:52 +0800

    Added design

--- Src/Presentation/BikeInventory.Kiosk/Controllers/HomeController.cs
+++ Src/Presentation/BikeInventory.Kiosk/Controllers/HomeController.cs
@@ -42,11 +42,6 @@ namespace BikeInventory.Kiosk.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
